046116f4c745b5a9576851d0c69ff28639808819
Попенчук Роман Петрович <RPPopenchuk@FED.COM.UA>
2022-08-30 16:36:08 +0300

    ch01 add component Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";  
+import { View, Text, StyleSheet } from "react-native";
+
+
+export const Navbar = (props) => {
+    return (
+        <View>
+            <Text>Todo App</Text>
+        </View>
+    )
+} 
+
+const styles = StyleSheet.create ({
+    
+
+})
